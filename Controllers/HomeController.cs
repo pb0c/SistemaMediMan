40949491361c1cef9d43484c528dc3439bf8c72f
pb0c <p.boc89@gmail.com>
2020-06-30 00:32:36 -0400

    home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SistemaMediMan.Controllers
 {
@@ -10,7 +6,7 @@ namespace SistemaMediMan.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title="Vista Calendario";
+            ViewBag.Title="LOGIN";
             return View();
         }
 
